cc53b8bc77b5eb9c1f44f7603b0564d8e900f7b1
gqueste <gabriel.queste@gmail.com>
2014-05-13 17:20:14 +0200

    Petite modif variable PC

--- src/test/OpcodeTest.java
+++ src/test/OpcodeTest.java
@@ -10,13 +10,13 @@ import chip8.Chip8;
 public class OpcodeTest {
 	
 	private Chip8 chip8;
-	private int pc;
+	private int pcTemoin;
 	private byte[][] displayTemoin;
 	
 	@Before
 	public void setUp() {
 		this.chip8 = new Chip8();
-		this.pc = chip8.getPC();
+		this.pcTemoin = chip8.getPC();
 		this.displayTemoin = new byte[64][32];
 		for(int i=0;i<64;i++){
 			for(int j=0;j<32;j++){
@@ -29,7 +29,7 @@ public class OpcodeTest {
 	public void test00E0() {
 		chip8.opcode(0x00E0);
 		assertEquals("L'écran doit etre nettoyé",this.displayTemoin, chip8.getDisplay());
-		assertEquals("PC n'' a pas été incrémenté", pc+2, chip8.getPC());
+		assertEquals("PC n'' a pas été incrémenté", pcTemoin+2, chip8.getPC());
 	}
 
 }
